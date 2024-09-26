27e542ecfdf9a8aa5351fc04b8e05ddeaa22b859
bharathsurya021 <bharathsurya021@gmail.com>
2024-09-27 01:38:58 +0530

    generate query

--- server/utils/common.js
+++ server/utils/common.js
@@ -13,4 +13,31 @@ export const validateObjectId = (id) => {
   return mongoose.Types.ObjectId.isValid(id);
 };
 
+export const getFilterQuery = (tags, author, keyword) => {
+  let filterQuery = {};
 
+  if (tags && tags.length > 0) {
+    filterQuery.tags = { $in: tags };
+  }
+
+  if (author) {
+    filterQuery.author = { $regex: new RegExp(author, 'i') };
+  }
+
+  if (keyword) {
+    filterQuery.$or = [
+      { title: { $regex: keyword, $options: 'i' } },
+      { description: { $regex: keyword, $options: 'i' } },
+    ];
+  }
+
+  return filterQuery;
+};
+
+export const getSortOptions = (sortBy, order) => {
+  const validSortFields = ['title', 'likes', 'createdAt'];
+  const sortField = validSortFields.includes(sortBy) ? sortBy : 'createdAt';
+  const sortOrder = order === 'asc' ? 1 : -1;
+
+  return { [sortField]: sortOrder };
+};
