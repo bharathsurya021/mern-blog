c716c36671b86b92038087315ca2ffe624a3b4e8
bharathsurya021 <bharathsurya021@gmail.com>
2024-09-26 15:07:55 +0530

    use routes and middleware

--- server/app.js
+++ server/app.js
@@ -2,10 +2,20 @@ import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv'
 import colors from 'colors'
-const app = express();
+import authRoutes from './routes/authRoutes.js'
+import { errorHandler, notFound } from './middleware/errormiddleware.js';
+
 dotenv.config()
-app.use(express.json());
 connectDB()
+
+const app = express();
+app.use(express.json());
+
+//routes
+app.use('/api/v1/users', authRoutes)
+//middlewares
+app.use(errorHandler)
+app.use(notFound)
 
 app.get('/', (req, res) => {
 	res.send('Hello World from app.js!');
