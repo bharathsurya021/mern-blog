fe8bdea9b0a702feab635c6bb0126877e51ee8ed
bharathsurya021 <bharathsurya021@gmail.com>
2024-09-26 17:47:51 +0530

    import post routes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv'
 import colors from 'colors'
 import authRoutes from './routes/authRoutes.js'
+import postRoutes from './routes/postRoutes.js'
 import { errorHandler, notFound } from './middleware/errormiddleware.js';
 
 dotenv.config()
@@ -13,13 +14,17 @@ app.use(express.json());
 
 //routes
 app.use('/api/v1/users', authRoutes)
-//middlewares
-app.use(errorHandler)
-app.use(notFound)
+app.use('/api/v1/posts', postRoutes)
 
 app.get('/', (req, res) => {
 	res.send('Hello World from app.js!');
 });
+
+//middlewares
+app.use(notFound)
+app.use(errorHandler)
+
+
 
 
 export default app;
